6190fc1b65d764c002f26589199dcbb3fd0d1931
carl-kwon <carl.kwon@mediwhale.com>
2024-05-24 13:43:15 +0900

    feat : person class

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-a = {
-    "name": "John",
-    "age": 30,
-    "city": "New York"
-}
+class person:
+    def __init__(self, name, age, city):
+        self.name = name
+        self.age = age
+        self.city = city
+
+    def print_person(self):
+        print(f"Name: {self.name}, Age: {self.age}, City: {self.city}")
+
+a = person("John", 30, "New York")
